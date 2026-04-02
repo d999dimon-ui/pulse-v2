0ea1245b7f4162771f4bf39923d08a70072fc3c7
d999dimon-ui <d999dimon@gmail.com>
2026-04-03 00:58:10 +0600

    Fix: update dynamic import with .then((mod) => mod.default)

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,10 @@ import Script from "next/script";
 import dynamic from "next/dynamic";
 import "./globals.css";
 
-const Web3Provider = dynamic(() => import("@/contexts/Web3Provider"), {
-  ssr: false,
-  loading: () => null,
-});
+const Web3Provider = dynamic(
+  () => import('./contexts/Web3Provider').then((mod) => mod.default),
+  { ssr: false }
+);
 
 export const metadata: Metadata = {
   title: "TaskHub - Earn with Tasks",
