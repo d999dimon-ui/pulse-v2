b2a8945e8535cc6a6bfc7f41b5d83b469e3fe237
d999dimon-ui <d999dimon@gmail.com>
2026-04-03 22:00:21 +0600

    Fix: disable Web3Provider to fix 'Origin not allowed' error from WalletConnect/TON Connect

--- app/client-layout.tsx
+++ app/client-layout.tsx
@@ -1,7 +1,10 @@
 'use client';
 
-import Web3Provider from '@/contexts/Web3Provider';
+// Web3Provider отключён из-за ошибки "Origin not allowed" от WalletConnect/TON Connect
+// Для включения: добавь домен в https://cloud.walletconnect.com и раскомментируй:
+// import Web3Provider from '@/contexts/Web3Provider';
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
-  return <Web3Provider>{children}</Web3Provider>;
+  // return <Web3Provider>{children}</Web3Provider>;
+  return <>{children}</>;
 }
